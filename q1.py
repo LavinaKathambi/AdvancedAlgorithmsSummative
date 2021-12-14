1fa1f034ff1a4c19227c85fbfdd32364be81da3d
lavinakathambi <lavinakathambi.m@gmail.com>
2021-12-14 12:12:25 +0200

    second

--- q1.py
+++ q1.py
@@ -1,5 +1,11 @@
-number = int(input("Enter the Number: "))
-sum = 0
-for value in range(1, number + 1):
-    sum = sum + value
-print(sum)
+# PYTHON ALGORITHM FOR SUM 
+inputNumber = int(input("Input a number for maximum range: "))
+# Initialize sum of elements to be 0 at the beginning
+totalSum = 0
+# loop through the range of array elements 
+# Increament the values to the total sum
+for num in range(1, inputNumber + 1):
+    # Increament the values to the total sum
+    totalSum = totalSum + num
+# Return the final total sum
+print(totalSum)
